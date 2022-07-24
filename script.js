f0c46c98b44d4a1528f239474fec1369fcb3b3ae
Vinícius M <viniqpoweryt@gmail.com>
2022-07-24 01:23:50 -0300

    Add styles and grid size/clear function

--- script.js
+++ script.js
@@ -3,20 +3,53 @@ put grid inside a container
 set up a hover effect that change colors when a mouse passes over them
 */
 containerGrid = document.querySelector('.containerGrid')
-for (let i = 1; i < 17; i++ ) {
-    let columnDiv = document.createElement('div')
-    columnDiv.classList.add('columnDiv')
-    columnDiv.textContent = ''
-    containerGrid.appendChild(columnDiv)
-    for (let j = 1; j < 17; j++) {
-     let lineDiv = document.createElement('div')
-     lineDiv.textContent = ''
-     lineDiv.classList.add('lineDiv')
-     lineDiv.addEventListener('mouseover', () => {
-        lineDiv.style.cssText = 'background-color: red;'
-     });
-     columnDiv.appendChild(lineDiv)
+
+let maxGrid = 17
+let squareSize = 500 / maxGrid
+grid(maxGrid)
+
+let clearButton = document.querySelector('.clearButton')
+clearButton.addEventListener('click', () => {
+    removeGrid()
+    grid(maxGrid)
+})
+
+let sizeButton = document.querySelector('.sizeButton')
+sizeButton.addEventListener('click', () => {
+    maxGrid = parseInt(prompt('Choose the size of Grid (.max 100)', ''))
+    maxGrid += 1
+    if (maxGrid < 17 || maxGrid > 101) {
+        maxGrid = 17
+    }
+    removeGrid()
+    grid(maxGrid)
+});
+
+
+
+
+
+function grid(size) {
+    for (let i = 1; i < maxGrid; i++ ) {
+        let columnDiv = document.createElement('div')
+        columnDiv.classList.add('columnDiv')
+        columnDiv.textContent = ''
+        containerGrid.appendChild(columnDiv)
+        for (let j = 1; j < maxGrid; j++) {
+        let lineDiv = document.createElement('div')
+        lineDiv.classList.add('lineDiv')
+        let squareSize = (500 / size)
+        lineDiv.style.cssText = `height: ${squareSize}px; width: ${squareSize}px;`
+        lineDiv.addEventListener('dragover', () => {
+            lineDiv.style.cssText = `background-color: black; height: ${squareSize}px; width: ${squareSize}px;`
+        });
+        columnDiv.appendChild(lineDiv)
+
+        }
 
     }
+}
 
+function removeGrid() {
+    document.querySelectorAll(".columnDiv").forEach(el => el.remove());
 }
