aa8c65f8ea4eae894ad2559253747ca7e1f3ead3
Vinícius M <viniqpoweryt@gmail.com>
2022-07-23 21:10:06 -0300

    Create a functional 'Etch a Sketch'

--- script.js
+++ script.js
@@ -2,3 +2,21 @@
 put grid inside a container
 set up a hover effect that change colors when a mouse passes over them
 */
+containerGrid = document.querySelector('.containerGrid')
+for (let i = 1; i < 17; i++ ) {
+    let columnDiv = document.createElement('div')
+    columnDiv.classList.add('columnDiv')
+    columnDiv.textContent = ''
+    containerGrid.appendChild(columnDiv)
+    for (let j = 1; j < 17; j++) {
+     let lineDiv = document.createElement('div')
+     lineDiv.textContent = ''
+     lineDiv.classList.add('lineDiv')
+     lineDiv.addEventListener('mouseover', () => {
+        lineDiv.style.cssText = 'background-color: red;'
+     });
+     columnDiv.appendChild(lineDiv)
+
+    }
+
+}
